1ad247290bbaef6d494573ae8bcbbaa1b11d8a1e
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-11 23:52:03 +0530

    Create 04. multiplication_table.cpp

---
+++ 04. Loops/04. multiplication_table.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number whose multiplication table you want to print : " << endl;
+    cin >> n;
+    for (int i = 1; i <= 10; i++)
+    {
+        /* code */
+        cout << n << " X " << i << " = " << n * i << endl;
+    }
+
+    return 0;
+}
