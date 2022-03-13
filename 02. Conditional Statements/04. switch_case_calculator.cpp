31cf471fac8acf6132c6047aa6267ad6e6aaf3f6
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-13 23:20:27 +0530

    Create 04. switch_case_calculator.cpp

---
+++ 02. Conditional Statements/04. switch_case_calculator.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "MENU" << endl;
+    cout << "1. Add \n"
+         << "2. Subtract \n"
+         << "3. Multiply \n"
+         << "4. Divide \n";
+    int option;
+    cin >> option;
+    int a, b, c;
+    cout << "Enter two numbers : ";
+    cin >> a >> b;
+    switch (option)
+    {
+    case 1:
+        /* code */
+        c = a + b;
+        cout << "Sum = " << c << endl;
+        break;
+    case 2:
+        c = a - b;
+        cout << "Difference = " << c << endl;
+        break;
+    case 3:
+        c = a * b;
+        cout << "Product = " << c << endl;
+        break;
+    case 4:
+        c = a / b;
+        cout << "Quotient = " << c << endl;
+        break;
+
+    default:
+        cout << "Invalid Choice";
+        break;
+    }
+    return 0;
+}
