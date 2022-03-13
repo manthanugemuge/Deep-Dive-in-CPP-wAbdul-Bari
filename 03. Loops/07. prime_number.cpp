c247d6babdf8746a88bd4e934812f39b748f8b7d
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-13 23:23:36 +0530

    Create 07. prime_number.cpp

---
+++ 03. Loops/07. prime_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter a number : ";
+    cin >> n;
+    int counter = 0;
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+            counter++;
+    }
+    if (counter > 2)
+    {
+        cout << "Given number " << n << " is not a prime number.";
+    }
+    else
+    {
+        cout << "Given number " << n << " is a prime number." << endl;
+    }
+
+    return 0;
+}
