d9676f6e51338be129c9eb142d7ddd43da0c0dc3
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-13 23:22:38 +0530

    Create 06. leap_year.cpp

---
+++ 02. Conditional Statements/06. leap_year.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int year ;
+    cout << "Enter the year you want to check for leap year : ";
+    cin >> year;
+    if (year % 4 == 0)
+    {
+        if (year % 100 == 0)
+        {
+            if (year % 400 == 0)
+                cout << "Year " << year << " is leap year.";
+            else
+                cout << "Year " << year << " is not a leap year.";
+        }
+        else
+            cout << "Year " << year << " is a leap year.";
+    }
+    else
+        cout << "Year " << year << " is not a leap year.";
+
+    return 0;
+}
