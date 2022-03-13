c40a0640f3496aa78ca959ccbbf000e7c276518f
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-13 23:39:06 +0530

    Create 11. palindrome.cpp

---
+++ 03. Loops/11. palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, rev = 0;
+    cout << "Enter a number : ";
+    cin >> n;
+    int n1 = n;
+    while (n1 != 0)
+    {
+        int r;
+        r = n1 % 10;
+        rev = rev * 10 + r;
+        n1 /= 10;
+    }
+    if (n == rev)
+        cout << "Number " << n << " is a palindrome.";
+    else
+    {
+        cout << "Number " << n << " is not a palindrome.";
+    }
+
+    return 0;
+}
