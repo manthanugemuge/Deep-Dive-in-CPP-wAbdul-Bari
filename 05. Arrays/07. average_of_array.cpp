91b72bcb0a733048062464c92362281d17d464e5
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-14 23:53:59 +0530

    Update 07. average_of_array.cpp

--- 05. Arrays/07. average_of_array.cpp
+++ 05. Arrays/07. average_of_array.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i;
+    float num[100], sum = 0.0, average;
+    cout << "Enter the number of elements : ";
+    cin >> n;
+    float d;
+    for (i = 0; i < n; ++i)
+    {
+        cout << i + 1 << ". Enter Number : ";
+        cin >> num[i];
+        sum += num[i];
+    }
+    average = sum / n;
+    cout << "Average is : " << average;
+    return 0;
+}
