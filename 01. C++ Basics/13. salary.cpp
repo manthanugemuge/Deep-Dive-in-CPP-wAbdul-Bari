85e44c7991d16dbc0f3e4f33ede33ba72a18b9d1
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-15 23:38:45 +0530

    Create 13. salary.cpp

---
+++ 01. C++ Basics/13. salary.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    typedef float salary;
+    salary basic, per_allow, per_deduct, net;
+    cout << "Enter Basic Salary : ";
+    cin >> basic;
+    cout << "Enter Percentage of Allowances : ";
+    cin >> per_allow;
+    cout << "Enter Percentage of Deductions : ";
+    cin >> per_deduct;
+    net = basic + basic * per_allow / 100 - basic * per_deduct / 100;
+    cout << "Net Salary is : " << net << endl;
+    return 0;
+}
