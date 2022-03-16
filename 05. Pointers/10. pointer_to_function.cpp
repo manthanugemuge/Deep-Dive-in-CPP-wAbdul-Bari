a41f75484377591bb8f95f777840ef4700a0d8f0
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-16 23:57:40 +0530

    Update 10. pointer_to_function.cpp

--- 05. Pointers/10. pointer_to_function.cpp
+++ 05. Pointers/10. pointer_to_function.cpp
@@ -27,4 +27,21 @@ int main()
     (*fp)(29, 21); // Expected return 21 as called on min now.
     return 0;
 }
+/*
+We know that we can have pointer to any primitive data type, we can also have pointer of type class.
+We can also have pointer to a function. Above we have display function not returning anything and having no parameters.
+If we want to call the function from main we can say display() and hello will be displayed.
+Now, instead of directly calling the function using a name we can also use a pointer to the function.
+We define a function to the pointer and we call it fp above. return_type(*function_pointer_name)(parameters)
 
+On declaring a pointer to a function everything is same as prototype of a function, but instead of function name we have
+pointer name in bracket with parameters in another bracket. 
+Initialization done using function name.
+Calling done by giving function pointer name inside () and pass required parameters if any.
+
+Where we call the same pointer on max and min function we see that this is somewhat like polymorphism, different
+functions are called as pointer points towards different functions. So, this is same name but different functions.
+
+This is like polymorphism. In function over-riding internally functional pointers are used to achieve run-time polymorphism
+using function over-riding. One function pointer can point to any function with same signature/prototype.
+*/
