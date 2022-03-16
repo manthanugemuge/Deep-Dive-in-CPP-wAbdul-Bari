58f90bc3e4a2ef1ff848bb2d5554de618564b54e
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-16 23:52:44 +0530

    Update main.cpp

--- Project - Banking System/main.cpp
+++ Project - Banking System/main.cpp
@@ -1,1 +1,136 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
+#include <map>
+using namespace std;
+#define MIN_BALANCE 500
+class InsufficientFunds
+{
+};
+class Account
+{
+private:
+    long accountNumber;
+    string firstName;
+    string lastName;
+    float balance;
+    static long NextAccountNumber;
+
+public:
+    Account() {}
+    Account(string fname, string lname, float balance);
+    long getAccNo() { return accountNumber; }
+    string getFirstName() { return firstName; }
+    string getLastName() { return lastName; }
+    float getBalance() { return balance; }
+    void Deposit(float amount);
+    void Withdraw(float amount);
+    static void setLastAccountNumber(long accountNumber);
+    static long getLastAccountNumber();
+    friend ofstream &operator<<(ofstream &ofs, Account &acc);
+    friend ifstream &operator>>(ifstream &ifs, Account &acc);
+    friend ostream &operator<<(ostream &os, Account &acc);
+};
+long Account::NextAccountNumber = 0;
+class Bank
+{
+private:
+    map<long, Account> accounts;
+
+public:
+    Bank();
+    Account OpenAccount(string fname, string lname, float balance);
+    Account BalanceEnquiry(long accountNumber);
+    Account Deposit(long accountNumber, float amount);
+    Account Withdraw(long accountNumber, float amount);
+    void CloseAccount(long accountNumber);
+    void ShowAllAccounts();
+    ~Bank();
+};
+int main()
+{
+    Bank b;
+    Account acc;
+    int choice;
+    string fname, lname;
+
+    long accountNumber;
+    float balance;
+    float amount;
+    cout << "***Banking System***" << endl;
+    do
+    {
+        cout << "\n\tSelect one option below ";
+        cout << "\n\t1 Open an Account";
+        cout << "\n\t2 Balance Enquiry";
+        cout << "\n\t3 Deposit";
+        cout << "\n\t4 Withdrawal";
+        cout << "\n\t5 Close an Account";
+        cout << "\n\t6 Show All Accounts";
+        cout << "\n\t7 Quit";
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter First Name: ";
+            cin >> fname;
+            cout << "Enter Last Name: ";
+            cin >> lname;
+            cout << "Enter initil Balance: ";
+            cin >> balance;
+            acc = b.OpenAccount(fname, lname, balance);
+            cout << endl
+                 << "Congradulation Account is Created" << endl;
+            cout << acc;
+            break;
+        case 2:
+            cout << "Enter Account Number:";
+            cin >> accountNumber;
+            acc = b.BalanceEnquiry(accountNumber);
+            cout << endl
+                 << "Your Account Details" << endl;
+            cout << acc;
+            break;
+        case 3:
+            cout << "Enter Account Number:";
+            cin >> accountNumber;
+            cout << "Enter Balance:";
+            cin >> amount;
+            acc = b.Deposit(accountNumber, amount);
+            cout << endl
+                 << "Amount is Deposited" << endl;
+            cout << acc;
+            break;
+        case 4:
+            cout << "Enter Account Number:";
+            cin >> accountNumber;
+            cout << "Enter Balance:";
+            cin >> amount;
+            acc = b.Withdraw(accountNumber, amount);
+            cout << endl
+                 << "Amount Withdrawn" << endl;
+            cout << acc;
+            break;
+        case 5:
+            cout << "Enter Account Number:";
+            cin >> accountNumber;
+            b.CloseAccount(accountNumber);
+            cout << endl
+                 << "Account is Closed" << endl;
+            cout << acc;
+        case 6:
+            b.ShowAllAccounts();
+            break;
+        case 7:
+            break;
+        default:
+            cout << "\nEnter corret choice";
+            exit(0);
+        }
+    } while (choice != 7);
+
+    return 0;
+}
 
