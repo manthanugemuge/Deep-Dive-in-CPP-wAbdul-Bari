fc0c3d107a567ccbaa08b4958754926087f1e9d7
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-17 18:03:10 +0530

    Update 06. pass_by_value.cpp

--- 06. Functions/06. pass_by_value.cpp
+++ 06. Functions/06. pass_by_value.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+void swap(int a, int b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "Output of formal parameters a and b after swap(a,b) : " << a << " " << b << endl;
+}
 
+int main()
+{
+    int x = 10, y = 20;
+    cout << "Value of actual parameters x and y before swap(x,y) : " << x << " " << y << endl;
+    swap(x, y);
+    cout << "Output of actual parameters after swap(x,y) : " << x << " " << y << endl; // LOL swap doesn't happens.
+    return 0;
+}
