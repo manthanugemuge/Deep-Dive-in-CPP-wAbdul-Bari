af58607ed263d83705b524a40a00da59974cf578
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-18 22:06:37 +0530

    Update 02. class_creation.cpp

--- 07. Intro to OOP/02. class_creation.cpp
+++ 07. Intro to OOP/02. class_creation.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+class Rectangle // Defining a class
+{
+public: // Everything inside a class if not specified becomes private.
+        // Put stuff in public for direct accessibility.
+    int length, breadth;
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+
+int main()
+{
+    Rectangle r1, r2; // Creating instances or objects from class Rectangle
+    r1.breadth = 5;
+    r1.length = 10;
+    r2.length = 20; // Using dot operator to access variables and allocate value
+    r2.breadth = 30;
+    cout << "Area of r1 is : " << r1.area() << endl;
+    cout << "Perimeter of r2 is : " << r2.perimeter();
+    return 0;
+}
