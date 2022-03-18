466dc0d68fe7674e83db282e9b289fa38cc5361c
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-18 23:20:25 +0530

    Update 08. function_in_class.cpp

--- 07. Intro to OOP/08. function_in_class.cpp
+++ 07. Intro to OOP/08. function_in_class.cpp
@@ -76,3 +76,25 @@ int main()
     return 0;
 }
 
+/*
+The functions we define above for a class these aren't mandatory but creating these in a class would lead
+to creation of a perfect class. Writing these methods is just same as following pep8 for python or can be said to have
+a good standard code guidelines which makes it easy for program in every way.
+
+So our class should have the following functions in our class irrespective of need to create a perfect class :
+
+1. Constructors
+1.1 Non parameterized constructor
+1.2 Parameterized constructor
+1.3 Copy constructor
+--------------------------------------------------------------
+2. Setter or mutators
+3. Accessors or Getters
+4. Actual functions useful for class called Facilitators
+5. Inspector function -> Checks based on some properties, mostly a bool
+6. Destructor function
+
+In a class we just write the function names and do not define the functions inside it without ellaborating.
+Functions are ellaborated outside the class by using scope resolution operators.
+ 
+*/
