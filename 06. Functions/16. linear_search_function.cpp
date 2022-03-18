f67df09c264f9610a5663b5c00b685736c02a893
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-19 00:41:42 +0530

    Update 16. linear_search_function.cpp

--- 06. Functions/16. linear_search_function.cpp
+++ 06. Functions/16. linear_search_function.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+int search(int A[], int n, int key)
 
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (key == A[i])
+            return i;
+    }
+    return 0;
+}
+
+int main()
+{
+
+    int Array[] = {23, 65, 89, 41, 69, 45, 91};
+    int number = 7;
+    cout << "Elements of array are : " << endl;
+    int i = 0;
+
+    while (i != 7)
+    {
+        cout << Array[i] << "\t";
+        i++;
+    }
+
+    cout << "\nEnter the element you want to search for : ";
+    int k;
+    cin >> k;
+    int index = search(Array, number, k);
+    cout << "Element " << k << " found at index " << index << endl;
+    return 0;
+}
