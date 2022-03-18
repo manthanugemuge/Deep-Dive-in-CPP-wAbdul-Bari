4332aa8b647accb9d59d9df201318b96c4d1cae5
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-19 00:18:42 +0530

    Update 09. return_by_address.cpp

--- 06. Functions/09. return_by_address.cpp
+++ 06. Functions/09. return_by_address.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int *fun(int size)
+{
+    int *p = new int[size];
+    for (int i = 0; i < size; i++)
+        p[i] = i + 1;
+    return p;
+}
+int main()
+{
+    int *ptr = fun(5); // Creates an array of size 5 in heap, initializes the elements and returns the pointer.
+    // ptr will point to 0th index element of array in heap memory.
+
+    return 0;
+}
 
