9d992a2623f1895e9a4e09e39df45dce95179cbf
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-19 00:18:53 +0530

    Update 09. return_by_address.cpp

--- 06. Functions/09. return_by_address.cpp
+++ 06. Functions/09. return_by_address.cpp
@@ -15,3 +15,17 @@ int main()
     return 0;
 }
 
+/*
+Return by Address kind of functions :  Functions can also return addresses. Functions can take adderesses as parameters
+                                    and in that case functions are called call by address kind of functions.
+
+Above we have a function named fun which returns size as a parameter. The function takes a pointer and creates an array
+of that size. As new is used then memory is created in the heap. When new is used memory is used inside the heap.
+Code creates an array in heap and p is pointer to that array.
+Then we have code to assign every index location a value more than its index. 0 will be 1. 1 will be 2 and nth index will
+have value n+1.
+Function till now has created the array, filled it with elements and now we return the address using return p.
+
+When we have any work with heap memory then we can use functions which return the address of that heap memory and it
+will be useful for calling function memory. So above is a method for return by address for a function.
+*/
