11438ea096d2eea0a02ff94485fabd5177d8cfc6
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-20 23:12:50 +0530

    Update 11. struct_class.cpp

--- 07. Intro to OOP/11. struct_class.cpp
+++ 07. Intro to OOP/11. struct_class.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+using namespace std;
+struct Demo
+{
+    int x, y;
+
+    void Display()
+    {
+        cout << x << " " << y << endl;
+    }
+};
+
+int main()
+{
+    Demo d;
+    d.x = 10;
+    d.y = 15;
+    d.Display();
+    return 0;
+}
 
