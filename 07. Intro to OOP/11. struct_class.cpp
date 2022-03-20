1996542f3681bd62b21bbf2c470776216cb1828f
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-20 23:13:22 +0530

    Update 11. struct_class.cpp

--- 07. Intro to OOP/11. struct_class.cpp
+++ 07. Intro to OOP/11. struct_class.cpp
@@ -19,3 +19,9 @@ int main()
     return 0;
 }
 
+/*
+Data members can't be private or protected in a structure as opposed to a class. Also in C we can't have functions inside
+structures. In C++ structure is somewhat like class. Difference between the two being in class everything is private by
+default, which is not the case for class. Difference being structure has everything by default as public and class has
+everything by default as private.
+*/
