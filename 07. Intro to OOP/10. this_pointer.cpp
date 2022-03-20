2b097969985f479ff144aa79cd4453d2e8119e68
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-20 23:10:27 +0530

    Update 10. this_pointer.cpp

--- 07. Intro to OOP/10. this_pointer.cpp
+++ 07. Intro to OOP/10. this_pointer.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+private:
+    int length, breadth;
+
+public:
+    Rectangle(int length, int breadth)
+    {
+        this->length = length;
+        this->breadth = breadth;
+    }
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+};
+
+int main()
+{
+    Rectangle r1(10, 5); 
+    return 0;
+}
 
