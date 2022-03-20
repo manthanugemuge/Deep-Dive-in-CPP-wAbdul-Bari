31205d908cf801b1044ff6dd806d10466c7b7510
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-20 23:11:13 +0530

    Update 10. this_pointer.cpp

--- 07. Intro to OOP/10. this_pointer.cpp
+++ 07. Intro to OOP/10. this_pointer.cpp
@@ -27,3 +27,20 @@ int main()
     return 0;
 }
 
+/*
+It would be more readable for us to have constructor which goes like
+
+Rectangle(int length, int breadth)
+    {
+        length = length;
+        breadth = breadth;
+    }
+//The above length = length won't make sense as we don't know which length is being assigned to which variable.
+To avoid ambiguity and make the name statement more clear we use this->length.
+this->length is the pointer to the length which is inside private in the class. Same goes for this->breadth the breadth
+pointer which is made private. And we assign the values which are being passed into the function to those.
+
+this-> pointer is used to remove the ambiguity between parameters of functions and data members of a class.
+this-> is used to refer to the data member of current class we are writing this in.
+After creating the object r1 this->length is read as length of r1 and same goes for breadth.
+*/
