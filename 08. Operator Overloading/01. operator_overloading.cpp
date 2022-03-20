ba7f13ac2182fee7224f570ce61c97ea0dd76cbd
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-20 23:19:55 +0530

    Update 01. operator_overloading.cpp

--- 08. Operator Overloading/01. operator_overloading.cpp
+++ 08. Operator Overloading/01. operator_overloading.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+private:
+    int real, img;
+
+public:
+    Complex(int real = 0, int img = 0) // 0 if nothing is passed
+    {
+        this->real = real;
+        this->img = img;
+    }
+    // Complex add(Complex x) // Normal way
+    Complex operator+(Complex x)
+    { // Signature of add function
+        Complex temp;
+        temp.real = this->real + x.real;
+        temp.img = this->img + x.img;
+        return temp;
+    }
+    int get_real()
+    {
+        return this->real;
+    }
+    int get_img()
+    {
+        return this->img;
+    }
+};
+
+int main()
+{
+    Complex c1(5, 7), c2(2, 9), c3;
+    // c3 = c1.add(c2); // Old method
+    c3 = c1 + c2; // Using operator overloading
+    cout << "Sum of c1(5,7) and c2(5,9) complex numbers is : " << c3.get_real() << " + " << c3.get_img() << "i as sum.";
+    return 0;
+}
+
 
