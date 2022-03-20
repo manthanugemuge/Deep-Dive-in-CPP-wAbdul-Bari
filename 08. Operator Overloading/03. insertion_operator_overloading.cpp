ae5bc014bb62663473ad37d8356e8e79cf2d3fd7
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-20 23:24:29 +0530

    Update 03. insertion_operator_overloading.cpp

--- 08. Operator Overloading/03. insertion_operator_overloading.cpp
+++ 08. Operator Overloading/03. insertion_operator_overloading.cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
 
+class Complex
+{
+private:
+    int real, img;
+
+public:
+    Complex(int real = 0, int img = 0) // 0 if nothing is passed
+    {
+        this->real = real;
+        this->img = img;
+    }
+    // Complex add(Complex x) // Normal way
+    Complex operator+(Complex x)
+    { // Signature of add function
+        Complex temp;
+        temp.real = this->real + x.real;
+        temp.img = this->img + x.img;
+        return temp;
+    }
+    int get_real()
+    {
+        return this->real;
+    }
+    int get_img()
+    {
+        return this->img;
+    }
+    /* void display() // To display a complex number, now we want cout << c1; to have result same as c1.display()
+    {//So, we have to overload the insertion operator, for that we build the function prototype
+        cout << this->real << " + " << this->img << "i";
+    } */
+    friend ostream &operator<<(ostream &o, Complex &c1); // Friend function has to be outside the class
+};
+
+int main()
+{
+    Complex c1(5, 7), c2(2, 9), c3;
+    // c3 = c1.add(c2); // Old method
+    c3 = c1 + c2; // Using operator overloading
+    cout << c1;
+    cout << c2;
+    cout << "Sum of c1(5,7) and c2(5,9) complex numbers is : " << c3.get_real() << " + " << c3.get_img() << "i as sum.";
+    return 0;
+}
+
+ostream &operator<<(ostream &o, Complex &c1)
+{
+    o << "Complex number is : ";
+    o << c1.real << " + " << c1.img << "i" << endl;
+    return o;
+}
