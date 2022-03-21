402dd7b44ae0687c637a3d9bf68f3a4d6fbbbab0
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-21 23:56:33 +0530

    Update 06. access_specifier.cpp

--- 09. Inheritance/06. access_specifier.cpp
+++ 09. Inheritance/06. access_specifier.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private: // Data members as private
+    int length;
+    int breadth;
+
+public: // Members functions as public
+    int area()
+    {
+        return length * breadth;
+    }
+    int perimeter()
+    {
+        return 2 * (length + breadth);
+    }
+    void set_length(int length)
+    {
+        (length > 0) ? this->length = length : this->length = 0;
+    }
+    void set_breadth(int breadth)
+    {
+        (breadth > 0) ? this->breadth = breadth : this->breadth = 0;
+    }
+    int get_length() { return this->length; }
+    int get_breadth() { return this->breadth; }
+};
+
 
