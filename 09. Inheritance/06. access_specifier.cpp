a4f18d7566f3060cf51a052a9693fd8f5c3b8dc4
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-21 23:56:40 +0530

    Update 06. access_specifier.cpp

--- 09. Inheritance/06. access_specifier.cpp
+++ 09. Inheritance/06. access_specifier.cpp
@@ -28,4 +28,13 @@ public: // Members functions as public
     int get_breadth() { return this->breadth; }
 };
 
-
+int main()
+{
+    Rectangle r1;
+    r1.set_breadth(5);
+    r1.set_length(6);
+    cout << "Area is : " << r1.area() << endl;
+    cout << "Length is : " << r1.get_length() << endl;
+    cout << "Breadth is : " << r1.get_breadth() << endl;
+    return 0;
+}
