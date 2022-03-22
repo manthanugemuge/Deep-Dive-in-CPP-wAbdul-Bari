fdf0b4a196811f224600979b8b3cb7fcabbea16a
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-22 23:52:27 +0530

    Update 03. base_ptr_der_object.cpp

--- 10. Base Class Pointer Derived Class Object/03. base_ptr_der_object.cpp
+++ 10. Base Class Pointer Derived Class Object/03. base_ptr_der_object.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+public:
+    void area()
+    {
+        cout << "Area of rectangle " << endl;
+    }
+};
+
+class Cuboid : public Rectangle
+{
+public:
+    void volume() { cout << "Volume of cuboid " << endl; }
+};
+
+int main()
+{
+    Cuboid c;
+    /* Able to call both of the functions
+    c.area();
+    c.volume();
+     */
+    // Creating pointer to rectangle class and to this we will assign the address of it's child class object i.e. c
+    Rectangle *p = &c;
+    p->area(); // Able to call the function area as it is defined in the class Rectangle.
+    // p->volume(); // Gives error that Rectangle has no member volume()
+    return 0;
+}
 
