4814b56436cf5d67e45cbf96681511be5126a281
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-23 23:45:47 +0530

    Update 05. abstract_class.cpp

--- 11. Polymorphism/05. abstract_class.cpp
+++ 11. Polymorphism/05. abstract_class.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+using namespace std;
+class Base
+{
+public:
+    void fun1()
+    {
+        cout << "Base fun1";
+    }
+    virtual void fun2() = 0;
+    /*void fun2() // Normal function fun2 we make it as pure virtual function above
+    {
+        cout << "Base fun2";
+    } 
+    */
+};
+
+class Derived : public Base
+{
+    // Derived class re-uses the function of base class -> Borrowing the features of base class -> Reusability
+    void fun2()
+    {
+        cout << "Derived Fun2"; // This is function overriding : Re-defining the function of base class in a child class
+    }
+
+};
+int main()
+{
+    Base *p = new Derived();
+    p->fun2(); // Prints derived fun2 to show that pure virtual function fun2 ignored of Base class and Derived is printed.
+    return 0;
+}
 
