aaa2ca82bd445222dd299c5e60509f4c5ff92b4f
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-23 23:36:16 +0530

    Update 01. function_overriding.cpp

--- 11. Polymorphism/01. function_overriding.cpp
+++ 11. Polymorphism/01. function_overriding.cpp
@@ -26,4 +26,15 @@ int main()
     return 0;
 }
 
+/*
+If we create object of Parent and call the function then function of parent will be called,
+if we create the function with same name in the child class and even if child inherits the
+parent publicly even then the function of child object will be called even though parent
+has the function with same name as the class child. Writing the function with same name
+is function overriding. Already parent class has a function and child class inherits from parent
+and borrows that function but is redefining the function itself, in a way as if it's their own
+version.
 
+In function overriding signature or prototype of the function stays same while there is a change in parameters in case
+of function overloading.
+*/
