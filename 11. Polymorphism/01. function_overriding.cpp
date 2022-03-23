95bee970cc6280f80f8fc334f8108993cdd092f0
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-23 23:36:09 +0530

    Update 01. function_overriding.cpp

--- 11. Polymorphism/01. function_overriding.cpp
+++ 11. Polymorphism/01. function_overriding.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+class Parent
+{
+public:
+    void display()
+    {
+        cout << "Display of parent." << endl;
+    }
+};
+
+class Child : public Parent
+{
+public:
+    void display()
+    {
+        cout << "Display of child." << endl;
+    }
+};
+
+int main()
+{
+    Child c;
+    c.display();
+    c.Parent::display(); // To call display function of Parent
+    return 0;
+}
+
 
