0a38158af1075d05895bd2cc0e0317ed0b4d29f1
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-25 00:01:47 +0530

    Update 05. static_members_example.cpp

--- 12. Friend and Static Member/05. static_members_example.cpp
+++ 12. Friend and Static Member/05. static_members_example.cpp
@@ -39,4 +39,25 @@ int main()
     cout << "Number of Admissions : " << Student::addNo;
 }
 
+// Example of Innova and Price of all objects remaining static
+/*class Innova
+{
+public:
+    static int price;
+    static int getPrice()
+    {
+        return price;
+    }
+};
 
+int Innova::price = 20;
+
+int main()
+{
+    Innova i1, i2, i3;
+    cout << i1.price << endl;
+    cout << i3.price << endl;
+
+    cout << Innova::price << endl;
+}
+ */
