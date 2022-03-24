f6aa588433a686cbe1f7649a235ad5159510a4ab
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-24 23:59:53 +0530

    Update 04. static_example.cpp

--- 12. Friend and Static Member/04. static_example.cpp
+++ 12. Friend and Static Member/04. static_example.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
+class Test
+{
+public:
+    int a;
+    static int count;
+
+    Test()
+    {
+        a = 10;
+        count++;
+    }
+    static int getCount()
+    {
+        return count;
+    }
+};
+
+int Test::count = 0;
+
+int main()
+{
+    Test t1, t2;
+    cout << t1.count << endl;
+    cout << t2.count << endl;
+    t1.count = 25; // Change value of t1 count and print value of t2 count yet it is same as t1.count
+    cout << t2.count << endl;
+    cout << Test::getCount() << endl;
+}
 
