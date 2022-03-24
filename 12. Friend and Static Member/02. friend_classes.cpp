398daa42d10fd8cb587a39bd490ca1326cac6212
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-24 23:55:52 +0530

    Update 02. friend_classes.cpp

--- 12. Friend and Static Member/02. friend_classes.cpp
+++ 12. Friend and Static Member/02. friend_classes.cpp
@@ -19,6 +19,17 @@ public:
     }
 };
 
+/*
+Friend classes : Above there is a class called My and has only one data member which is private and is a.
+Another class called your exists which takes an object of class My and public, (has a relationship)
+Upon object m created in Your class publicly no data member can be accessed, that's why we make the class your as friend.
+
+To allow one class to access private members of another class upon object then we make class Your as friend of class My.
+
+This concept is useful in container classes. Your has an object of class m so Your class is container of objects of My class.
+So, in container classes if we want to access private or protected members then what we do is we declare them as friend
+inside those classes.
+*/
 
 int main()
 {
