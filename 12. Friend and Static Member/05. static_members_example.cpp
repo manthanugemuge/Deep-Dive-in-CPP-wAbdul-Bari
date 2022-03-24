fe49fccb8aae15293ae60369644d5347d2da2da8
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-25 00:01:40 +0530

    Update 05. static_members_example.cpp

--- 12. Friend and Static Member/05. static_members_example.cpp
+++ 12. Friend and Static Member/05. static_members_example.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Example of students in a school and their static admission number as Roll No.
+
+class Student
+{
+public:
+    int roll;
+    string name;
+
+    static int addNo;
+    Student(string name)
+    {
+        addNo++;
+        roll = addNo;
+        this->name = name;
+    }
+    void display()
+    {
+        cout << "Name : " << name << endl;
+        cout << "Roll No. : " << roll << endl;
+    }
+};
+
+int Student::addNo = 0;
+
+int main()
+{
+    Student s1("Peter");
+    Student s2("John");
+    Student s3("Cosmic");
+    Student s4("Matthew");
+    Student s5("Eric");
+    Student s6("Deborah");
+
+    s1.display();
+    s3.display();
+    cout << "Number of Admissions : " << Student::addNo;
+}
+
 
