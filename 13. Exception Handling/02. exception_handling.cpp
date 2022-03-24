7b427c253e220e32ad5369cd2df3b69170b50d24
Manthan Ugemuge <87055332+ManthanUgemuge@users.noreply.github.com>
2022-03-25 00:07:16 +0530

    Update 02. exception_handling.cpp

--- 13. Exception Handling/02. exception_handling.cpp
+++ 13. Exception Handling/02. exception_handling.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x = 10, y = 0, z;
+    try
+    {
+        if (y == 0)
+            throw 0;
+        z = x / y;
+        cout << z << endl;
+    }
+    catch (int e)
+    {
+        cout << "Division by zero...!! Error Code: " << e << endl;
+    }
+    cout << "Outside try catch";
+    return 0;
+}
 
